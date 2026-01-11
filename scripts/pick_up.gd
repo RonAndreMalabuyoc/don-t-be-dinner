ffd4d0d873f03d84c7a3c5218b14c63bc872dd5a
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2026-01-11 16:32:24 +0800

    Can now pick up fruit
    Now the player can pick up fruit. It just doesn't look like it visually. It stays on screen even though its picked up

--- scripts/pick_up.gd
+++ scripts/pick_up.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-@export var item_id: String = "bomb"
-@export var duration := 5.0
+@export var item_id: String = "fruit"
+@export var duration: float = 5.0
 
-func _on_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("push_powerup"):
+		print("Picked up:", item_id)
 		body.call("push_powerup", item_id, duration, true)
-		queue_free()
+		get_parent().queue_free() # removes the whole pickup
+	print("Touched by:", body.name)
