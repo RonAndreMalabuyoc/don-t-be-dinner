7622f8477c2031ef15cb8c8d1d086bccfcb50e7f
Maria Teresa Mae B. De Jesus <mariateresamaedj19@gmail.com>
2026-01-18 10:57:55 +0800

    ui_changes
    pause button not fixed yet (temporary details yet to be changed)

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,3 +6,18 @@ func _ready():
 func _exit_tree():
 	Global.node_creation_parent = null
 	
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MAIN.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
