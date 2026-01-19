be05b6dd4a9d31c55f7f5885aa6defe62346d370
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2026-01-19 23:37:16 +0800

    Banana explosion

--- scenes/Bullets/banana_explosion.gd
+++ scenes/Bullets/banana_explosion.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-@onready var anim := get_node_or_null("Anim") as AnimatedSprite2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	if anim == null:
-		push_error("BananaExplosion: Missing AnimatedSprite2D child named 'Anim'")
-		queue_free()
-		return
-
 	anim.play("explode")
 	anim.animation_finished.connect(_on_anim_finished)
 
